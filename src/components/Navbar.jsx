bab84c87ee5ebb863d366ab0e3ec021443fd6319
batatacodes <batatacodes@gmail.com>
2025-08-17 14:43:06 -0300

    Add files via upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav>
+      <a href="#inicio">Início</a>
+      <a href="#conceitos">Conceitos</a>
+      <a href="#diagnostico">Diagnóstico</a>
+      <a href="#intervencoes">Intervenções</a>
+      <a href="#educacao">Educação</a>
+      <a href="#vida-adulta">Vida Adulta</a>
+      <a href="#direitos">Direitos</a>
+      <a href="#mitos">Mitos</a>
+      <a href="#recursos">Recursos</a>
+      <a href="#glossario">Glossário</a>
+    </nav>
+  )
+}
+
+export default Navbar
